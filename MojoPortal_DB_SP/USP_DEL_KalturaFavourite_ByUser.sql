500e9b64347a7ca113eed63944a6a187ad0f4199
Ranjan <biswas.1981@gmail.com>
2018-07-08 03:40:56 +0530

    SP create

---
+++ MojoPortal_DB_SP/USP_DEL_KalturaFavourite_ByUser.sql
@@ -0,0 +1,44 @@
+USE [mojoportaldb]
+GO
+/****** Object:  StoredProcedure [dbo].[Usp_KalturaMedia_SetFavourites]    Script Date: 08-07-2018 03:17:54 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--[dbo].[USP_DEL_KalturaFavourite_ByUser] '1_ra8t8647', 1
+
+CREATE PROCEDURE [dbo].[USP_DEL_KalturaFavourite_ByUser] 
+
+/*
+
+Author:   			Ranjan Biswas
+
+Created: 			08-07-2018
+
+Last Modified: 		08-07-2018
+
+*/
+
+@MediaID nvarchar(20),
+
+@UserID int
+
+
+
+AS
+
+
+
+Declare @MediaGUID uniqueidentifier
+
+
+
+EXEC [dbo].[Usp_KalturaMedia_GetGuid] @MediaID, NULL
+
+
+
+SELECT @MediaGUID = [MediaGUID] FROM [dbo].[Usr_TblKalturaMediaMapping] WHERE [MediaID]=@MediaID;
+
+DELETE FROM [dbo].[Usr_TblKalturaUserFavourites]  WHERE  [dbo].[Usr_TblKalturaUserFavourites].MediaGUID = @MediaGUID AND [dbo].[Usr_TblKalturaUserFavourites].UserID = @UserID
+
